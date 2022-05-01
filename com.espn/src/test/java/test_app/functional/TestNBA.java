32dcf78911471b22dabf08502111fcc01592983f
asharif02 <asharif02@gmail.com>
2022-05-01 02:14:30 -0400

    Added TestNBA and TestBasePage class. Also added data in espn_data excel file

---
+++ com.espn/src/test/java/test_app/functional/TestNBA.java
@@ -0,0 +1,8 @@
+package test_app.functional;
+
+import test_base.TestBasePage;
+
+public class TestNBA extends TestBasePage {
+
+
+}
---
+++ com.espn/src/test/java/test_base/TestBasePage.java
@@ -0,0 +1,10 @@
+package test_base;
+
+import base.BasePage;
+import utils.Database;
+
+public class TestBasePage extends BasePage {
+
+    public Database database;
+
+}
