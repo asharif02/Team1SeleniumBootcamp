647010b43b6d183f557ebf2b379302fc7b359001
asharif02 <asharif02@gmail.com>
2022-05-07 02:11:41 -0400

    Pushing 3 verizon test cases. Need to fix findHighestPrice method

--- com.verizon/src/main/java/app/pom/phone_accessories/CaseBundles.java
+++ com.verizon/src/main/java/app/pom/phone_accessories/CaseBundles.java
@@ -1,4 +1,4 @@
-package app.pom;
+package app.pom.phone_accessories;
 
 import app.shared.SystemBar;
 import org.openqa.selenium.WebElement;
@@ -7,12 +7,12 @@ import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
-public class PhoneCases extends SystemBar {
+public class CaseBundles extends SystemBar {
 
     @FindBy(xpath = "//div[@class='Tile__Pricing-sc-71g958-10 fClbGb blur']/p")
     public List<WebElement> prices;
 
-    public void findCheapestPrice(){
+    public void findLowestPrice(){
         double min = Double.MAX_VALUE;
         WebElement cheapestElement = null;
 
@@ -33,8 +33,28 @@ public class PhoneCases extends SystemBar {
         cheapestElement.click();
     }
 
-    public PhoneCases(){
+//    public void findHighestPrice(){
+//        double min = Double.MAX_VALUE;
+//        WebElement cheapestElement = null;
+//
+//        for(WebElement element : prices){
+//
+//            StringBuilder sb = new StringBuilder(element.getText());
+//            sb.deleteCharAt(0);
+//            String price = sb.toString();
+//            System.out.println(price);
+//            double doublePrice = Double.parseDouble(price);
+//            if(doublePrice > min){
+//                min = doublePrice;
+//                cheapestElement = element;
+//            }
+//        }
+//
+//        System.out.println("THIS IS THE MAXIMUM FOUND! " + min);
+//        cheapestElement.click();
+//    }
+
+    public CaseBundles() {
         PageFactory.initElements(driver, this);
     }
-
 }
--- com.verizon/src/test/java/test_app/smoke/TestAccessories.java
+++ com.verizon/src/test/java/test_app/smoke/TestAccessories.java
@@ -2,24 +2,40 @@ package test_app.smoke;
 
 import app.pom.AccessoriesPage;
 import app.pom.Homepage;
-import app.pom.PhoneCases;
-import org.checkerframework.checker.units.qual.A;
-import org.openqa.selenium.WebElement;
+import app.pom.phone_accessories.CaseBundles;
+import app.pom.phone_accessories.PhoneCases;
 import org.testng.annotations.Test;
 import base_test.BaseTest;
-
-import java.util.List;
 
 public class TestAccessories extends BaseTest {
 
     @Test
-    public void testFirstLink() {
+    public void testPhoneCasesLink() {
         Homepage homepage = new Homepage();
         homepage.clickAccessoriesLink();
         AccessoriesPage ap = new AccessoriesPage();
         ap.clickPhoneAccessoriesColumnCategory("cases");
         PhoneCases phoneCases = new PhoneCases();
         phoneCases.findCheapestPrice();
-
     }
+
+    @Test
+    public void testFindLowestPhoneCaseBundle() {
+        Homepage homepage = new Homepage();
+        homepage.clickAccessoriesLink();
+        AccessoriesPage ap = new AccessoriesPage();
+        ap.clickPhoneAccessoriesColumnCategory("case bundles");
+        CaseBundles caseBundles = new CaseBundles();
+        caseBundles.findLowestPrice();
+    }
+
+//    @Test
+//    public void testFindHighestPhoneCaseBundle() {
+//        Homepage homepage = new Homepage();
+//        homepage.clickAccessoriesLink();
+//        AccessoriesPage ap = new AccessoriesPage();
+//        ap.clickPhoneAccessoriesColumnCategory("case bundles");
+//        CaseBundles caseBundles = new CaseBundles();
+//        caseBundles.findHighestPrice();
+//    }
 }
